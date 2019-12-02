01f1d21dad5104da37b8616750ff526225342a34
Alamag <artem.baev.2001@yandex.ru>
2019-12-03 01:40:03 +0300

    Ready
    Готова только вёрстка

--- app/src/main/java/com/example/pumplife/SignUpActivity.kt
+++ app/src/main/java/com/example/pumplife/SignUpActivity.kt
@@ -3,10 +3,10 @@ package com.example.pumplife
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RegActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
     }
 }
