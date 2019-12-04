0d3631c7b666388d6c54c58bc39528184da2174f
Alamag <artem.baev.2001@yandex.ru>
2019-12-04 03:35:00 +0300

    Changes

--- app/src/main/java/com/example/pumplife/SignInActivity.kt
+++ app/src/main/java/com/example/pumplife/SignInActivity.kt
@@ -1,12 +1,42 @@
 package com.example.pumplife
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        go_to_sign_up_button.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        sign_in_button.setOnClickListener{
+            val email = edit_email.text.toString()
+            val password = edit_pass.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+
+                //Вход пользователя по бд
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else Toast.makeText(this, "Введите почту и пароль", Toast.LENGTH_LONG).show()
+        }
+
+        stop_button.setOnClickListener{
+
+            //Выход из программы
+
+            finish()
+        }
     }
+
+
+
 }
--- app/src/main/java/com/example/pumplife/SignUpActivity.kt
+++ app/src/main/java/com/example/pumplife/SignUpActivity.kt
@@ -1,12 +1,37 @@
 package com.example.pumplife
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        go_to_sign_in_button.setOnClickListener{
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        sign_up_button.setOnClickListener{
+            val email = edit_email.text.toString()
+            val password = edit_pass.text.toString()
+            val confPass = repeat_pass.text.toString()
+            if (password == confPass && password.isNotEmpty() && email.isNotEmpty()) {
+
+                //Добавление пользователя в бд
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else if (password == confPass) {
+                Toast.makeText(this, "Все поля должны быть заполнены", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(this, "Пароли не совпадают", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
