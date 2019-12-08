83b4d7b44e60948093759376532abb8809ef06a8
Takexito <Takexito@gmail.com>
2019-12-08 19:24:38 +0300

    card ui fix

--- app/src/main/java/com/example/pumplife/CourseActivity.kt
+++ app/src/main/java/com/example/pumplife/CourseActivity.kt
@@ -3,9 +3,6 @@ package com.example.pumplife
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pumplife.controller.CourseManager
-import com.example.pumplife.controller.UsersDB
-import com.example.pumplife.model.Card
-import com.example.pumplife.model.Course
 import com.example.pumplife.model.TheorCard
 import com.example.pumplife.ui.courses.TheorCardFragment
 import kotlinx.android.synthetic.main.activity_course.*
@@ -28,11 +25,11 @@ class CourseActivity : AppCompatActivity() {
             card = null
             supportFragmentManager.beginTransaction().replace(R.id.courseFragment, TheorCardFragment()).addToBackStack(null).commit()
         }
-        button.setOnClickListener{
+        closeButton.setOnClickListener{
             close()
         }
+        progressBar.progress = course.percent()
     }
-
     fun next() {
         if (cardID++ >= course.cardList.size - 1){
             card = null
@@ -42,15 +39,17 @@ class CourseActivity : AppCompatActivity() {
             card = course.cardList[cardID]
             if(cardID >= course.completedCardNum) course.completedCardNum++
         }
+        progressBar.progress = course.percent()
         supportFragmentManager.beginTransaction().replace(R.id.courseFragment, TheorCardFragment()).commit()
     }
 
-    fun prev(){
-        if (cardID <= 0) card = null
-        else card = course.cardList[cardID--]
-
-        supportFragmentManager.beginTransaction().replace(R.id.courseFragment, TheorCardFragment()).commit()
-    }
+//    fun prev(){
+//        if (cardID <= 0) card = null
+//        else card = course.cardList[cardID-1]
+//        cardID -= 1
+//
+//        supportFragmentManager.beginTransaction().replace(R.id.courseFragment, TheorCardFragment()).commit()
+//    }
 
     fun close() {
         super.finish()
--- app/src/main/java/com/example/pumplife/ui/courses/TheorCardFragment.kt
+++ app/src/main/java/com/example/pumplife/ui/courses/TheorCardFragment.kt
@@ -43,5 +43,6 @@ class TheorCardFragment: Fragment() {
                 activity.next()
             }
         }
+
     }
 }
